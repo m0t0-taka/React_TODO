6949f7c8fd0bd1dc9d06031ad76d3990e1ababb3
m0t0-taka <81753585+m0t0-taka@users.noreply.github.com>
2021-11-13 18:36:15 +0900

    finish

---
+++ src/components/InputTodo.jsx
@@ -0,0 +1,20 @@
+const style = {
+  backgroundColor: "#c1ffff"
+};
+
+export const InputTodo = (props) => {
+  const { todoText, onChange, onClick, disabled } = props;
+  return (
+    <div style={style}>
+      <input
+        disabled={disabled}
+        placeholder="TODOを入力"
+        value={todoText}
+        onChange={onChange}
+      />
+      <button disabled={disabled} onClick={onClick}>
+        追加
+      </button>
+    </div>
+  );
+};
